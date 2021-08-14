3065277e3fb6ff22d8ee0525529e169203312d71
ks40698 <77614375+ks40698@users.noreply.github.com>
2021-08-14 17:14:00 +0530

    Update download_aria_p_n.py

--- publicleechgroup/helper_funcs/download_aria_p_n.py
+++ publicleechgroup/helper_funcs/download_aria_p_n.py
@@ -203,7 +203,7 @@ async def fake_etairporpa_call(
         await sent_message_to_update_tg_p.reply_text(
             "Your file is uploaded in the GDrive\n"
             "You can find your file here"
-            f"\n\nhttps://hx-index.herokuapp.com/AnyDl/TorToolKitLeech/"
+            f"\n\nhttps://hx-index.herokuapp.com/AnyDl/PublicLeech/"
         )
         return True, None
 
